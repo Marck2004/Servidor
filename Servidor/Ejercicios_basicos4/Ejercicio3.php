0943b65455bc4af97cff2af4a539ada5fb89e6d3
Marck2004 <124670526+Marck2004@users.noreply.github.com>
2023-09-24 20:41:58 +0200

    Add files via upload

---
+++ Servidor/Ejercicios_basicos4/Ejercicio3.php
@@ -0,0 +1,55 @@
+
+    <?php
+
+    /*ha obtenido una pareja de dados iguales de mayor valor, si los
+    dos han obtenido parejas distintas
+    ha obtenido una pareja de dados iguales, si el otro jugador no
+    ha obtenido pareja
+   ha obtenido una puntuación total mayor, si ningún jugador ha
+    obtenido pareja*/
+
+        $dado1 = rand(1,6);
+        $dado2 = rand(1,6);
+        $dado3 = rand(1,6);
+        $dado4 = rand(1,6);
+
+            $sumaprimera = $dado1 + $dado2;
+            $sumasegunda = $dado3 + $dado4;
+
+        echo "<div style='border:6px solid red;width: 284px;height: 140px;float:left;'>
+        <img src='img/$dado1.svg'>
+        <img src='img/$dado2.svg'></div>
+
+            <div style='border:6px solid blue;width: 284px;height: 140px;float:left;'>
+            <img src='img/$dado3.svg'>
+            <img src='img/$dado4.svg'></div>";
+        
+    switch (!is_null($dado1)) {
+        case $dado1==$dado2 && $dado3!=$dado4:
+            print "Ha ganado el primer jugador";
+        break;
+        case $dado1!=$dado2 && $dado3==$dado4:
+            print "Ha ganado el segundo jugador";
+        break;
+        case $dado1=$dado2 > $dado3=$dado4:
+            print "Ha ganado el primer jugador";
+        break;
+        case $sumaprimera > $sumasegunda:
+            print "Ha ganado el primer jugador";
+        break;
+
+        case $dado3=$dado4 > $dado1=$dado2:
+            print "Ha ganado el segundo jugador";
+        break;
+        case $sumaprimera < $sumasegunda:
+            print "Ha ganado el segundo jugador";
+        break;
+            default: 
+        print "Han quedado empate";
+        break;
+    
+    }
+    
+
+    ?>
+    
