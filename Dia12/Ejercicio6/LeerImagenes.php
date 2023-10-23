092cefc02686f377187f6065c296b5b1aff24532
Marcos Cobo <marcosmaestrocobo@gmail.com>
2023-10-23 16:36:12 +0200

    directorios y ficheros hecho

--- Dia12/Ejercicio6/LeerImagenes.php
+++ Dia12/Ejercicio6/LeerImagenes.php
@@ -2,20 +2,22 @@
 
     $abrirFichero = fopen("infopelis.txt","a");
     $abrirDirectorio = opendir("imagenesPeliculas");
-
-    $abrirCopia = opendir("copiaImg");
+        mkdir("copiaImg/");
+    $abrirCopia = opendir("copiaImg/");
+    
         if(!$abrirFichero) die ("EROR: no se pudo abrir el archivo");
 
         while($archivo = readdir($abrirDirectorio)){
             if(!is_dir($archivo)){
+                $archivoDestino = "copiaImg/" . $archivo;
             fwrite ($abrirFichero,$archivo);
             $tamaño = "  Tamaño: ".filesize("imagenesPeliculas/".$archivo);
             fwrite ($abrirFichero,$tamaño."\n");
-            copy($archivo,"copiaImg/");
+            copy("imagenesPeliculas/".$archivo,$archivoDestino);
         }
     }
     
-    
+    closedir($abrirCopia);
     closedir($abrirDirectorio);
     fclose($abrirFichero);
 
